4ae8c0a7cf5db4346facb11adb0892d6690c02be
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-18 10:10:11 +0800

    Update index.py

--- index.py
+++ index.py
@@ -3,8 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 # domain root
-@app.route('/')
-def home():
-    return 'Hello, World!'
+@app.route("/", methods=["GET", "POST"])
+def callback():
+    return "Hello World!!"
 
-
+ 
