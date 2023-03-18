69fecebac07b0e71e75290799dd723a116182da1
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-19 05:03:40 +0800

    Create

---
+++ appbackup.py
@@ -0,0 +1,14 @@
+from flask import Flask #載入Flask
+app=Flask(__name__) #建立application 物件
+
+#建立網站首頁的回應方式
+@app.route("/") 
+def index(): #用來回應網頁首頁連線的函式
+    return "hello flask!!!!!吃早餐了"
+
+@app.route("/page1")
+def info():
+    return "相關資訊"
+#啟動伺服器
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=3000)
