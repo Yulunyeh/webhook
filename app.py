a0ebd644a3002418fce43a07fc26d87b020a043d
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-18 10:29:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-from flask import Flask 
+from flask import Flask #載入Flask
+app=Flask(__name__) #建立application 物件
 
-app = Flask(__name__)
+#建立網站首頁的回應方式
+@app.route("/") 
+def index(): #用來回應網頁首頁連線的函式
+    return "hello flask!!!!!吃早餐了"
 
-# domain root
-@app.route("/", methods=["POST"])
-def callback():
-    return "Hello World!!"
+@app.route("/page1")
+def info():
+    return "相關資訊"
+#啟動伺服器
+app.run()
