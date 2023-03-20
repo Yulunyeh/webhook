df59ee609ebde6bb3e1b5f7e1cdaa6d2d208b4fd
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-20 16:23:09 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,11 @@ app=Flask(__name__) #建立application 物件
 #建立網站首頁的回應方式
 @app.route("/") 
 def index(): #用來回應網頁首頁連線的函式
-    return "hello flask!!!!!吃早餐了"
+    return "hello flasklab!!<br>"
 
 @app.route("/page1")
 def info():
-    return "相關資訊"
+    return "相關資訊<br>"
 #啟動伺服器
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=3000)
