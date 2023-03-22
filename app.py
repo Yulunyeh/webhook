e54aa8f19b459eaec3b8048023e4d4fc90dcc541
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-22 21:00:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -44,14 +44,14 @@ def handle_message(event):
         prompt = event.message.text
         def generate_response(prompt):
             response = openai.Completion.create(
-            engine="text-davinci-003",
-            prompt=prompt,
-            max_tokens=150,
-            n=1,
-            stop=None,
-            temperature=0.7,
-        )
-        aireply = response.choices[0].text 
+                            engine="text-davinci-003",
+                            prompt=prompt,
+                            max_tokens=150,
+                            n=1,
+                            stop=None,
+                            temperature=0.7,
+                        )
+            aireply = response.choices[0].text 
           
 
         # Send To Line             
