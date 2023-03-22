658e6393c252729820b79ac47232286417d1b30c
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-22 10:52:51 +0800

    Update app.py

--- app.py
+++ app.py
@@ -59,7 +59,7 @@ def handle_message(event):
         # Send To Line
         # reply= generate_response(get_message)
         
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=get_message))
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=event.message.text))
     
    
 
