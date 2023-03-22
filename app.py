1b7f53b1d39cce59f45957657242ede6e17bd69d
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-22 10:48:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -4,15 +4,12 @@ from linebot import LineBotApi, WebhookHandler
 from linebot.exceptions import InvalidSignatureError
 from linebot.models import MessageEvent, TextMessage, TextSendMessage
 import openai
-import configparser
-import requests
+
 
 app=Flask(__name__) #建立application 物件
 line_bot_api = LineBotApi("cjdsY5UFfcFL5iFWtKGBgZA5XuRNimer8zhHqGWGg4pDzzVIBEe+CahXeBYQQm9oN6JoVhgUhw0rxSEgJ4Fz1x9lNBv4cTMrEMfAJtNs4mFXTOlDcFnKNikw4VDDMT0gImC875xyja2RRbCTBropEgdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("ae1838d725d0d9321c4336c7ffda695f")
 openai.api_key = "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"
-OPENAI_KEY = openai.api_key
-CHATGPT_URL = "https://api.openai.com/v1/completions"
 
 #建立網站首頁的回應方式
 @app.route("/") 
@@ -39,39 +36,30 @@ def callback():
             abort(400)
 
         return "OK"
+
+def generate_response(prompt):
+    response = openai.Completion.create(
+        engine="text-davinci-003",
+        prompt=prompt,
+        max_tokens=150,
+        n=1,
+        stop=None,
+        temperature=0.7,
+    )
+
+    generated_text = response.choices[0].text 
+    return geterated_text
     
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.source.user_id != "ae1838d725d0d9321c4336c7ffda695f":
-         # model parameter
-        data = {
-                "model": "text-davinci-003",
-                "prompt": event.message.text,
-                 "max_tokens": 4000,
-                 "temperature": 0.9,
-                 # "top_p": 1,
-                 # "n": 1,
-                 # "stream": False,
-                 # "logprobs": None,
-                 # "stop": "\n"
-                }
-
-        # API from open ai
-        response = requests.post(
-                CHATGPT_URL,
-                headers={
-                    'Content-Type': 'application/json',
-                    'Authorization': " ".join(["Bearer", OPENAI_KEY])
-                    },
-            json=data)
-        res_json = response.json()
-        reply_text = res_json.get("choices")[0].get("text").replace("\n", "").replace("?", "")
+        get_message = event.message.text
     
-        # Reply the text to client
-        line_bot_api.reply_message(
-                event.reply_token,
-                TextSendMessage(text=reply_text))
 
+        # Send To Line
+        # reply= generate_response(get_message)
+        
+        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=get_message))
     
    
 
