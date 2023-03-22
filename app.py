e4e6df7278a756b6c45014c4d18ecdaa7edfe5dc
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-22 21:24:22 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ app=Flask(__name__) #建立application 物件
 line_bot_api = LineBotApi("cjdsY5UFfcFL5iFWtKGBgZA5XuRNimer8zhHqGWGg4pDzzVIBEe+CahXeBYQQm9oN6JoVhgUhw0rxSEgJ4Fz1x9lNBv4cTMrEMfAJtNs4mFXTOlDcFnKNikw4VDDMT0gImC875xyja2RRbCTBropEgdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("ae1838d725d0d9321c4336c7ffda695f")
 openai.api_key = "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"
-aireply=""
 
 #建立網站首頁的回應方式
 @app.route("/") 
@@ -42,7 +41,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     if event.source.user_id != "ae1838d725d0d9321c4336c7ffda695f":
-        prompt = event.message.text
+        promptx = event.message.text
         def generate_response(prompt):
             response = openai.Completion.create(
                             engine="text-davinci-003",
@@ -53,10 +52,10 @@ def handle_message(event):
                             temperature=0.7,
                         )
             aireply = response.choices[0].text 
-          
-
-        # Send To Line             
-        line_bot_api.reply_message(event.reply_token, TextSendMessage(text=aireply))
+            return aireply
+    # Send To Line 
+    reply = generate_response(promptx)
+    line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply))
     
 #啟動伺服器
 if __name__ == "__main__":
