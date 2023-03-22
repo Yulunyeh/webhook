49053924485de69de88c397cc0a104fd5a1f92f9
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-22 16:51:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -38,7 +38,14 @@ def callback():
         return "OK"
 
 
-    
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    reply = event.message.text
+    print(reply)
+    line_bot_api.reply_message(
+            event.reply_token, 
+            TextSendMessage(text=reply)
+    )    
 
     
    
