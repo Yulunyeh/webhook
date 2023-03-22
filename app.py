72843341018fb8aedde7aa14e1c03c838de1ec2f
Yulunyeh <Yulunyeh@hotmail.com>
2023-03-22 17:36:00 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ import openai
 app=Flask(__name__) #建立application 物件
 line_bot_api = LineBotApi("cjdsY5UFfcFL5iFWtKGBgZA5XuRNimer8zhHqGWGg4pDzzVIBEe+CahXeBYQQm9oN6JoVhgUhw0rxSEgJ4Fz1x9lNBv4cTMrEMfAJtNs4mFXTOlDcFnKNikw4VDDMT0gImC875xyja2RRbCTBropEgdB04t89/1O/w1cDnyilFU=")
 handler = WebhookHandler("ae1838d725d0d9321c4336c7ffda695f")
-openai.api_key = "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"
+openai.api_key = "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"
 
 #建立網站首頁的回應方式
 @app.route("/") 
